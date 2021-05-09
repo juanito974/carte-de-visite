4941dd20cf9d0ca01926fb26b82edc0afe54ba2d
BOYER Juanito <psdh772@corp.log.intra.laposte.fr>
2021-05-09 02:15:38 +0200

    Mise à jour HTML et CSS,  ajout d'un JS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('test');
+
+function bigImg(x) {
+  x.style.height = "300px";
+  x.style.width = "300px";
+  
+}
+
+function normalImg(x) {
+  x.style.height = "60px";
+  x.style.width = "60px";
+}
+
